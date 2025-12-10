2e1a264065910088e47a4378b611a376f7b4beeb
Matteo Lovato <matteo.lovato@allievi.itsdigitalacademy.com>
2025-12-10 23:00:04 +0100

    aggiunta esercizi con csv

---
+++ studenti/mlovato/csv/death_valley_highs_lows.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+from pathlib import Path
+import csv
+from datetime import datetime
+
+path = Path("weather_data/death_valley_2021_simple.csv")
+lines = path.read_text(encoding="utf-8").splitlines()
+
+reader = csv.reader(lines)
+header_row = next(reader)
+
+dates, highs, lows = [], [], []
+# riprende a leggere da dove ha iniziato
+for row in reader:
+    current_date = datetime.strptime(row[2], "%Y-%m-%d")
+    try:
+        high = int(row[3])
+        low = int(row[4])
+    except ValueError:
+        print(f"Dati mancanti per {current_date}")
+    else:
+        dates.append(current_date)
+        highs.append(high)
+        lows.append(low)
+
+# plotta le temperature
+plt.style.use("seaborn-v0_8")
+fig, ax = plt.subplots()
+ax.plot(dates, highs, color="red", alpha=0.5)
+ax.plot(dates, lows, color="blue", alpha=0.5)
+ax.fill_between(dates,highs,lows, facecolor="blue", alpha=0.1)
+fig.autofmt_xdate()
+
+# cambiare le label
+ax.set_title("Daily High and Lows Temperatures, 2021\nDeath Valley, CA", fontsize=20)
+ax.set_xlabel("", fontsize=16)
+ax.set_ylabel("Temperature (F)", fontsize=16)
+ax.tick_params(labelsize=16)
+
+plt.show()
--- studenti/mlovato/csv/sitka_highs_lows.py
+++ studenti/mlovato/csv/sitka_highs_lows.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 from datetime import datetime
 
+from pyparsing import alphas
+
 path = Path("weather_data/sitka_weather_2021_simple.csv")
 
 lines = path.read_text(encoding="utf-8").splitlines()
@@ -11,22 +13,26 @@ reader = csv.reader(lines)
 # legge la prima riga
 header_row = next(reader)
 
-dates, highs = [], []
+dates, highs, lows = [], [], []
 # riprende a leggere da dove ha iniziato
 for row in reader:
     current_date = datetime.strptime(row[2], "%Y-%m-%d")
     high = int(row[4])
+    low = int(row[5])
     dates.append(current_date)
     highs.append(high)
+    lows.append(low)
 
 # plotta le temperature
 plt.style.use("seaborn-v0_8")
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color="red")
+ax.plot(dates, highs, color="red", alpha=0.5)
+ax.plot(dates, lows, color="blue", alpha=0.5)
+ax.fill_between(dates,highs,lows, facecolor="blue", alpha=0.1)
 fig.autofmt_xdate()
 
 # cambiare le label
-ax.set_title("Daily High Temperatures, 2021", fontsize=24)
+ax.set_title("Daily High and Lows Temperatures, 2021", fontsize=24)
 ax.set_xlabel("", fontsize=16)
 ax.set_ylabel("Temperature (F)", fontsize=16)
 ax.tick_params(labelsize=16)
